293edeebfccd28ba014c9b51f53151c78f41eec1
Tommy Tran <tommyktran@gmail.com>
2021-11-19 06:49:53 -0500

    Add project files.

---
+++ ChessSearch/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//function getGame(play = "", ratings = '1600,1800,2000,2200,2500', speeds = 'blitz,rapid,classical') {
+//    fetch(`https://explorer.lichess.ovh/lichess?variant=standard&speeds=${speeds}&play=${play}&ratings=${ratings}`)
+//        .then(response => response.json())
+//        .then((data) => {
+//            console.log(data);
+//            return data;
+//        });
+//};
+
+//getGame();
